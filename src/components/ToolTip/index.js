4aa93af7de2dc760346f8144f6a1b196b3098751
Tandy Sean <tandysean@gmail.com>
2018-06-26 12:57:49 -0400

    updated to handle off screen issues

--- src/components/ToolTip/index.js
+++ src/components/ToolTip/index.js
@@ -20,10 +20,27 @@ class ToolTip extends Component {
   }
 
   setToolTipPosition() {
+    let pageWidth = document.documentElement.offsetWidth;
     let triggerWidth = this.toolTipTrigger.offsetWidth;
     let toolTipWidth = this.toolTipWindow.offsetWidth;
     let triggerLeftOffset = this.toolTipTrigger.offsetLeft;
     let offsetWindow = Math.ceil((triggerLeftOffset + (triggerWidth / 2)) - (toolTipWidth / 2 ));
+    let arrowOffset = - offsetWindow;
+    if (offsetWindow <= 0) {
+      offsetWindow = 0;
+      this.topArrow.style.left = arrowOffset + 'px';
+    } else {
+      this.topArrow.style.left = toolTipWidth / 2 + 'px';
+    }
+
+    let rightOffset = toolTipWidth + offsetWindow;
+
+    if(rightOffset > pageWidth) {
+      offsetWindow = pageWidth - toolTipWidth;
+      let currentArrowPos = parseInt(this.topArrow.style.left);
+      let adjustedArrowOffset = rightOffset - pageWidth;
+      this.topArrow.style.left = currentArrowPos + adjustedArrowOffset + 'px';
+    }
     this.toolTipWindow.style.left = offsetWindow + 'px';
   }
 
@@ -33,7 +50,7 @@ class ToolTip extends Component {
         <span href="#tool-tip"  onClick={this.handleClick} ref={(ref) => this.toolTipTrigger = ref } className="tool-tip-trigger">Where's my PIN?</span>
         <div id="#tool-tip" ref={ (ref) => this.toolTipWindow = ref } className={`tool-tip-window columns ${this.state.showToolTip ? "" : "hidden"}`}>
           <button onClick={this.handleClick}className="close-button hide-text">close</button>
-          <div className="top-arrow">▲</div>
+          <div className="top-arrow" ref={ (ref)=> this.topArrow = ref }>▲</div>
           <div className="columns">
             <div className="column is-one-quarter">
               <img width="75" src={toolTipImage} />
